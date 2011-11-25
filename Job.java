6a1f4c398a6bac9876a0b55680bd23ac86cb90ee
Chris L <stylesuxx@gmail.com>
2011-11-25 16:47:50 +0100

    added Javadoc

--- Job.java
+++ Job.java
@@ -1,15 +1,29 @@
 import org.jivesoftware.smack.Chat;
 
+/** This class represents a Job
+  */
 public class Job{
   private String message, nick, resource;
   private Chat chat = null;
 
+  /** Default MUC Constructor
+    * @param message A Jobs message 
+    * @param nick Who sent the message
+    * @param resource Where is the message comming from
+    */
   public Job( String message, String nick, String resource){
     this.message = message;
     this.nick = nick;
     this.resource = resource;
   }
 
+
+  /** Default private Constructor
+    * @param message A Jobs message 
+    * @param nick Who sent the message
+    * @param resource Where is the message comming from
+    * @param chat Chat Object
+    */
   public Job( String message, String nick, String resource, Chat chat){
     this.message = message;
     this.nick = nick;
@@ -17,8 +31,23 @@ public class Job{
     this.chat = chat;
   }
 
+  /** Return the Jobs message
+    * @return String
+    */
   public String getMessage(){ return message; }
+
+  /** Return the Jobs Nick
+    * @return String
+    */
   public String getNick(){ return nick; }
+
+  /** Return the Jobs Resource
+    * @return String
+    */
   public String getResource(){ return resource; }
+
+  /** Return the Jobs chat
+    * @return chat
+    */
   public Chat getChat(){ return chat; }
 }
