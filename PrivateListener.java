ddcb99cc5ac08915310f4fd5102c6f796bcc4ba6
Chris L <stylesuxx@gmail.com>
2011-11-29 17:28:07 +0100

    updated jevadoc

--- PrivateListener.java
+++ PrivateListener.java
@@ -3,11 +3,15 @@ import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.Chat;
 
 /** This Class handles all incomming private messages
+  *
+  * @author stylesuxx
+  * @version 0.1 
   */
 class PrivateListener implements MessageListener{
   ProcessInput pi;
 
   /** Default Constructor
+    * 
     * @param pc ProcessInput Queue to use
     */
   public PrivateListener( ProcessInput pi ){
@@ -15,6 +19,7 @@ class PrivateListener implements MessageListener{
   }
 
   /** Process incomming private messages
+    * 
     * @param chat Chat Object to talk back to user
     * @param m Message to process
     */
