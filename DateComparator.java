3c2df931628b05f6d987a0c8d4d512c7d74aba60
Chris L <stylesuxx@gmail.com>
2011-11-29 16:03:30 +0100

    Updated Javadoc

--- DateComparator.java
+++ DateComparator.java
@@ -1,15 +1,34 @@
-import java.util.*;
+import java.util.Date;
+import java.util.Map;
+import java.util.Comparator;
 
-class DateComparator implements Comparator {
+/** Date Comparator
+  *
+  * @author stylesuxx
+  * @version 0.1
+  */
+public class DateComparator implements Comparator<String> {
+  private Map<String,Date> base;
 
-  Map base;
-  public DateComparator( Map base ) {
+  /** Default Constructor
+    * 
+    * @param base (Unordered) Map to look up the values
+    */
+  public DateComparator( Map<String,Date> base ) {
       this.base = base;
   }
 
-  public int compare( Object a, Object b ){    
-    Date aa = (Date)base.get(a);
-    Date bb = (Date)base.get(b);
+  /** Compare 2 Dates
+    * 
+    * @param a String to Date 1
+    * @param b String to Date 2
+    * 
+    * @return int
+    */
+  public int compare( String a, String b ){    
+    Date aa = base.get(a);
+    Date bb = base.get(b);
     return aa.compareTo(bb);
   }
+
 }
